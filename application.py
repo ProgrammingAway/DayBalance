fc3043334326aa8f4eaa5b4084913982f8e01c68
Brian Ladner <github@theladners.com>
2020-09-29 01:29:33 +0000

    Initial update

---
+++ application.py
@@ -0,0 +1,191 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+import calendar
+from datetime import date, datetime, timedelta
+
+from helpers import apology, login_required, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show balance calendar"""
+
+    first_weekday = 6 # 0 = Monday, 6 = Sunday
+    current_calendar = calendar.Calendar(first_weekday)
+    weekdays_headers = []
+    for weekday in current_calendar.iterweekdays():
+        weekdays_headers.append(calendar.day_abbr[weekday])
+    todays_date = datetime.date(datetime.now())
+    change_months = 0
+    change_years = 0
+
+    # User reached route via POST (as by submitting a form via POST)
+#    if request.method == "POST":
+
+        # Ensure password was submitted
+#        elif request.form.get("password") != request.form.get("confirmation"):
+#            return apology("password does not match confirmation", 403)
+
+    # User reached route via GET (as by clicking a link or via redirect)
+#    else:
+
+    current_month = todays_date.month + change_months
+    current_year = todays_date.year + change_years
+    current_months_name = calendar.month_name[current_month]
+    month_days = current_calendar.itermonthdates(current_year, current_month)
+
+    return render_template("index.html",
+        todays_date=todays_date,
+        current_month=current_month,
+        current_year=current_year,
+        current_months_name=current_months_name,
+        weekdays_headers=weekdays_headers,
+        month_days=month_days,
+    )
+
+
+@app.route("/graph", methods=["GET", "POST"])
+@login_required
+def graph():
+    """Graph daily balance."""
+    return apology("TODO")
+
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    return apology("TODO")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        flash('You were successfully logged in')
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("confirmation"):
+            return apology("must provide password confirmation", 403)
+
+        # Ensure password was submitted
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("password does not match confirmation", 403)
+
+        # TODO: Check password rules!
+
+        # Query database for username
+        rows = db.execute("INSERT INTO users (username, hash) VALUES (:username, :pwhash)",
+                          username=request.form.get("username"),
+                          pwhash = generate_password_hash(request.form.get("password")))
+
+        # Redirect user to home page
+        flash('Registration successful')
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("register.html")
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)

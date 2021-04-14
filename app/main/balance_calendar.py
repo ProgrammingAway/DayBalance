a2ae03bf9c51878864bd07538f067b697ed8ef3a
Brian Ladner <github@theladners.com>
2021-04-14 14:51:08 -0500

    Update balance_calendar.py

--- app/main/balance_calendar.py
+++ app/main/balance_calendar.py
@@ -21,11 +21,11 @@ class BalanceCalendar(calendar.Calendar):
             self.weekday_headers.append(calendar.day_abbr[weekday])
 
     def starting balance(self, month, year):
-        month_day1 = list(self.itermonthdates(year, month))[0]
+        month_start_day = list(self.itermonthdates(year, month))[0]
         prev_transactions = Transaction.query.filter(
             Transaction.user_id == current_user.id,
             Transaction.date >= current_user.start_date, 
-            Transaction.date < month_day1
+            Transaction.date < month_start_day
         )
 
         # find transactions between current_user.start_date to 
@@ -40,5 +40,11 @@ class BalanceCalendar(calendar.Calendar):
                 month_start_balance = month_start_balance - transaction.amount
         return month_start_balance
 
-    def month_transactions():
-        pass
+    def month_transactions(self, month, year):
+        month_start_day = list(self.itermonthdates(year, month))[0]
+        month_end_day = list(self.itermonthdates(year, month))[-1]
+        return Transaction.query.filter(
+            Transaction.user_id == current_user.id,
+            Transaction.date >= month_start_day,
+            Transaction.date <= month_end_day,
+        )

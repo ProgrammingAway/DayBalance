c3f2861a08d73f7c13ceb0c2036d174e112f2d03
Brian Ladner <github@theladners.com>
2021-04-15 15:53:24 -0500

    Update forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,11 +8,15 @@ from flask_login import current_user
 class TransactionForm(Form):
     title = StringField('Transaction Name', validators=[DataRequired()])
     date = DateField('Transaction Date', validators=[DataRequired()])
-    amount = DecimalField('Transaction Amount', validators=[DataRequired()],
-        places=2)
-    description = TextAreaField('Description',
-        validators=[Length(min=0, max=140)])
+    amount = DecimalField('Transaction Amount', validators=[DataRequired()], places=2)
+    description = TextAreaField('Description', validators=[Length(min=0, max=140)])
     income = BooleanField('Income')
+#    is_recurring = BooleanField('Recurring')
+#    freq = SelectField('Frequency', choices=[('Yearly'), ('Monthly'), ('Weekly'), ('Daily')])
+#    interval = DecimalField('Frequency Interval', validators=[NumberRange(min=1, max=100], places=0)
+#    wkst = SelectMultipleField('Day of the Week', choices=[('SU'), ('MO'), ('TU'), ('WE'), ('TH'), ('FR'), ('SA')])
+#    count = DecimalField('Number of Transactions', validators=[NumberRange(min=1, max=100], places=0)  # (Cannot be used with until)
+#    until = DateField('End Date')  # (Cannot be used with count)
     submit = SubmitField('Submit')
 
     def validate_date(self, date):
@@ -23,3 +27,11 @@ class TransactionForm(Form):
                 + str(start_date) + '.'
             )
 
+    def validate_until(self, until):
+        if self.count.data and until.data:
+            raise ValidationError("Select 'End Date' or 'Number of Transactions', but not both.")
+        if until.data < self.date.data:
+            raise ValidationError('Please use an end date after the transaction date.')
+            
+    def validate_count(self, count):
+        

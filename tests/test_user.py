1134c5474cb5cf2f920ec9bcfadaa4b7e3060999
Brian Ladner <github@theladners.com>
2021-04-19 17:31:35 -0500

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -96,10 +96,10 @@ class UserModelCase(unittest.TestCase):
         t3 = self.createTestTransaction(user_id=u.id, date=date(2021,6,22), amount='20', income=True)
         t4 = self.createTestTransaction(user_id=u.id, date=date(2021,7,4), amount='12.36', income=False)
         # 500-100-30.22+20=389.78 For June calendar
-        july_start_balance = u.start_balance - u.month_starting_balance(2021, 7)
+        july_start_balance = u.start_balance - float(u.month_starting_balance(2021, 7))
         self.assertEqual(389.78, july_start_balance)
         # 389.78-12.36=377.42 For July calendar
-        august_start_balance = u.start_balance - u.month_starting_balance(2021, 8)
+        august_start_balance = u.start_balance - float(u.month_starting_balance(2021, 8))
         self.assertEqual(377.42, august_start_balance)
 
     def test_month_transactions(self):

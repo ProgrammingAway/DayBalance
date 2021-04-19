5d78f562921a7685efdf788bdd48f8ad64f3cbcf
Brian Ladner <github@theladners.com>
2021-04-19 16:06:25 -0500

    Update test_user.py

--- tests/test_user.py
+++ tests/test_user.py
@@ -18,32 +18,49 @@ class UserModelCase(unittest.TestCase):
         self.app_context.push()
         db.create_all()
 
+    def createTestUser(self):
+        u = User(username='brian')
+        u.set_password('password')
+        u.email = 'brian@test.com'
+        u.start_date = datetime.now()
+        u.start_balance = 10.00
+        db.session.add(u)
+        db.session.commit()
+
     def tearDown(self):
         db.session.remove()
         db.drop_all()
         self.app_context.pop()
 
     def test_password_hashing(self):
-        u = User(username='brian')
-        u.set_password('cat')
-        self.assertFalse(u.check_password('dog'))
-        self.assertTrue(u.check_password('cat'))
+        #u = User(username='brian')
+        #u.set_password('password')
+        self.createTestUser()
+        u = User.query.filter_by(username='brian').first()
+        self.assertFalse(u.check_password('other_password'))
+        self.assertTrue(u.check_password('password'))
 
     def test_token(self):
-        u = User(username='brian')
-        u.set_password('cat')
-        u.email = 'brian@test.com'
-        u.start_date = datetime.now()
-        u.start_balance = 10.00
-        db.session.add(u)
-        db.session.commit()
-        user = User.query.filter_by(username='brian').first()
-        token = user.get_reset_password_token()
+        #u = User(username='brian')
+        #u.set_password('password')
+        #u.email = 'brian@test.com'
+        #u.start_date = datetime.now()
+        #u.start_balance = 10.00
+        #db.session.add(u)
+        #db.session.commit()
+        self.createTestUser()
+        u = User.query.filter_by(username='brian').first()
+        token = u.get_reset_password_token()
         new_u = User.verify_reset_password_token(token)
-        self.assertEqual(user, new_u)
+        new_token = token[0:] + (token[-1] + 1)
+        new2_u = User.verify_reset_password_token(new_token)
+        self.assertEqual(u, new_u)
+        self.assertNotEqual(u, new2_u)
 
     def test_month_name(self):
-        u = User(username='brian')
+        #u = User(username='brian')
+        self.createTestUser()
+        u = User.query.filter_by(username='brian').first()
         months = { 1:"January", 2:"February", 3:"March", 4:"April", 5:"May",
             6:"June", 7:"July", 8:"August", 9:"September", 10:"October",
             11:"November", 12:"December"}
@@ -51,7 +68,9 @@ class UserModelCase(unittest.TestCase):
             self.assertEqual(name, u.month_name(num))
 
     def test_month_day(self):
-        u = User(username='brian')
+        #u = User(username='brian')
+        self.createTestUser()
+        u = User.query.filter_by(username='brian').first()
         dates = u.month_days(2020, 2)
         day2_29 = date(2020, 2, 29)
         day3_1 = date(2020, 3, 1)
@@ -59,7 +78,9 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(day3_1 in dates)
 
     def test_month_starting_balance(self):
-        u = User(username='brian')
+        #u = User(username='brian')
+        self.createTestUser()
+        u = User.query.filter_by(username='brian').first()
         pass
 
 if __name__ == '__main__':

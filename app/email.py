0d8aa1d5b4ba027748061a39308ddd0f8978e90f
Brian Ladner <github@theladners.com>
2021-04-23 10:37:33 -0500

    Update email.py

--- app/email.py
+++ app/email.py
@@ -1,24 +1,24 @@
-from threading import Thread
-from flask import current_app
-from flask_mail import Message
-from app import mail
+import threading
+import flask
+import flask_mail
+import app
 
 
 def send_async_email(app, msg):
     with app.app_context():
-        mail.send(msg)
+        app.mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body,
                attachments=None, sync=False):
-    msg = Message(subject, sender=sender, recipients=recipients)
+    msg = flask_mail.Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
     if attachments:
         for attachment in attachments:
             msg.attach(*attachment)
     if sync:
-        mail.send(msg)
+        app.mail.send(msg)
     else:
-        Thread(target=send_async_email,
-            args=(current_app._get_current_object(), msg)).start()
+        threading.Thread(target=send_async_email,
+            args=(flask.current_app._get_current_object(), msg)).start()

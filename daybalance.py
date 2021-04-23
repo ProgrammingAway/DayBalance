b73df965a15e20900179e7af43a300fc155aeff2
Brian Ladner <github@theladners.com>
2021-04-23 10:45:31 -0500

    Update daybalance.py

--- daybalance.py
+++ daybalance.py
@@ -1,9 +1,9 @@
-from app import create_app, db
-from app.models import User, Transaction
+import app
+import app.models
 
-app = create_app()
+app = app.create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Transaction': Transaction}
+    return {'db': app.db, 'User': app.models.User, 'Transaction': app.models.Transaction}

4fb9a783196a5c0ed7a9731a81bf31554a880881
Brian Ladner <github@theladners.com>
2021-04-23 14:12:52 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app, db
 from app.models import User, Transaction
 from config import Config

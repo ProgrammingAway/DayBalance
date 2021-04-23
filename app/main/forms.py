8668130e825785e50af18effa41cba69016deaf7
Brian Ladner <git@theladners.com>
2021-04-22 22:01:12 -0500

    fixed formatting

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,15 +8,50 @@ from flask_login import current_user
 class TransactionForm(Form):
     title = StringField('Transaction Name', validators=[InputRequired()])
     date = DateField('Transaction Date', validators=[InputRequired()])
-    amount = DecimalField('Transaction Amount', validators=[InputRequired()], places=2)
-    description = TextAreaField('Description', validators=[Length(min=0, max=140)])
+    amount = DecimalField(
+        'Transaction Amount',
+        validators=[InputRequired()],
+        places=2,
+    )
+    description = TextAreaField(
+        'Description',
+        validators=[Length(min=0, max=140)],
+    )
     income = BooleanField('Income')
     is_recurring = BooleanField('Recurring')
-    freq = SelectField('Frequency', choices=[('YEARLY', 'Year(s)'), ('MONTHLY', 'Month(s)'), ('WEEKLY', 'Week(s)'), ('DAILY', 'Day(s)')], validators=[Optional()])
-    interval = IntegerField('Frequency Interval', validators=[NumberRange(min=1, max=100), Optional()])
-    byweekday = SelectMultipleField('Day of the Week', choices=[('SU', 'Sunday'), ('MO', 'Monday'), ('TU', 'Tuesday'), ('WE', 'Wednesday'), ('TH', 'Thursday'), ('FR', 'Friday'), ('SA', 'Saturday')], validators=[Optional()])
+    freq = SelectField(
+        'Frequency',
+        choices=[
+            ('YEARLY', 'Year(s)'),
+            ('MONTHLY', 'Month(s)'),
+            ('WEEKLY', 'Week(s)'),
+            ('DAILY', 'Day(s)'),
+        ],
+        validators=[Optional()],
+        default='MONTHLY',
+    )
+    interval = IntegerField(
+        'Frequency Interval',
+        validators=[NumberRange(min=1, max=100), Optional()],
+    )
+    byweekday = SelectMultipleField(
+        'Day of the Week',
+        choices=[
+            ('SU', 'Sunday'),
+            ('MO', 'Monday'),
+            ('TU', 'Tuesday'),
+            ('WE', 'Wednesday'),
+            ('TH', 'Thursday'),
+            ('FR', 'Friday'),
+            ('SA', 'Saturday'),
+        ],
+        validators=[Optional()],
+    )
     # Cannot use until and count together
-    count = IntegerField('Number of Transactions', validators=[NumberRange(min=1, max=100), Optional()])
+    count = IntegerField(
+        'Number of Transactions',
+        validators=[NumberRange(min=1, max=100), Optional()],
+    )
     until = DateField('End Date', validators=[Optional()])
     submit = SubmitField('Submit')
 

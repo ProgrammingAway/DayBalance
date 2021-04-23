6493350ec68dda38556219552239ae8f92dc6cb1
Brian Ladner <github@theladners.com>
2021-04-23 14:21:45 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-import app import
+import app
 import app.models
 import config
 import datetime

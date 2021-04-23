f1c5c144ef3df7ac20235e9895afd6c752f850ab
Brian Ladner <github@theladners.com>
2021-04-23 10:48:01 -0500

    Update email.py

--- app/auth/email.py
+++ app/auth/email.py
@@ -1,18 +1,18 @@
-from flask import render_template, current_app
-from app.email import send_email
+import flask
+import app.email
 
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    send_email('[DayBalance] Reset Your Password',
-        sender=current_app.config['ADMINS'][0],
+    app.email.send_email('[DayBalance] Reset Your Password',
+        sender=flask.current_app.config['ADMINS'][0],
         recipients=[user.email],
-        text_body=render_template(
+        text_body=flask.render_template(
             'email/reset_password.txt',
             user=user,
             token=token,
         ),
-        html_body=render_template(
+        html_body=flask.render_template(
             'email/reset_password.html',
             user=user,
             token=token,

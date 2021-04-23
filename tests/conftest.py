4f2f3ee383d80587ccd0c17a41f7b924dcfe6f1d
Brian Ladner <github@theladners.com>
2021-04-23 14:20:44 -0500

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,21 +3,21 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import create_app, db
-from app.models import User, Transaction
-from config import Config
-from datetime import datetime, date
+import app import
+import app.models
+import config
+import datetime
 import pytest
 
 
-class TestConfig(Config):
+class TestConfig(config.Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     SECRET_KEY = 'testing'
     START_MONDAY = True
 
 
-class TestConfigSun(Config):
+class TestConfigSun(config.Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
     SECRET_KEY = 'testing'
@@ -26,14 +26,14 @@ class TestConfigSun(Config):
 
 @pytest.fixture(scope="function")
 def setup_db():
-    app = create_app(TestConfig)
+    app = app.create_app(TestConfig)
     app_context = app.app_context()
     app_context.push()
-    db.create_all()
+    app.db.create_all()
     yield
     # Tear Down
-    db.session.remove()
-    db.drop_all()
+    app.db.session.remove()
+    app.db.drop_all()
     app_context.pop()
 
 @pytest.fixture(scope="function")
@@ -41,45 +41,45 @@ def setup_db_sun():
     app = create_app(TestConfigSun)
     app_context = app.app_context()
     app_context.push()
-    db.create_all()
+    app.db.create_all()
     yield
     # Tear Down
-    db.session.remove()
-    db.drop_all()
+    app.db.session.remove()
+    app.db.drop_all()
     app_context.pop()
 
 @pytest.fixture(scope="function")
 def db_one_user(setup_db):
     setup_db
-    u = User(username='Panda')
+    u = app.models.User(username='Panda')
     u.set_password('password')
     u.email = 'panda@email.com'
-    u.start_date = date(2021, 1, 1)
+    u.start_date = datetime.date(2021, 1, 1)
     u.set_start_balance(1234.56)
-    db.session.add(u)
-    db.session.commit()
+    app.db.session.add(u)
+    app.db.session.commit()
     yield
     
 @pytest.fixture(scope="function")
 def db_one_user_sun(setup_db_sun):
     setup_db_sun
-    u = User(username='Panda')
+    u = app.models.User(username='Panda')
     u.set_password('password')
     u.email = 'panda@email.com'
-    u.start_date = date(2021, 1, 1)
+    u.start_date =  datetime.date(2021, 1, 1)
     u.set_start_balance(1234.56)
-    db.session.add(u)
-    db.session.commit()
+    app.db.session.add(u)
+    app.db.session.commit()
     yield
     
 @pytest.fixture(scope="function")
 def db_two_users(db_one_user):
     db_one_user
-    u = User(username='Oreo')
+    u = app.models.User(username='Oreo')
     u.set_password('password2')
     u.email = 'oreo@email.com'
     u.start_date = date(2021, 2, 1)
     u.set_start_balance(6543.21)
-    db.session.add(u)
-    db.session.commit()
+    app.db.session.add(u)
+    app.db.session.commit()
     yield

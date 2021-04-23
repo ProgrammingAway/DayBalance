953b937d8c2502fb4d5222c1f7289ffc69a667ea
Brian Ladner <github@theladners.com>
2021-04-23 13:33:21 -0500

    Update handlers.py

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,14 +1,15 @@
 import flask
 import app
-import app.errors
+from app.errors import bp
+#import app.errors
 
 
-@app.errors.bp.app_errorhandler(404)
+@bp.app_errorhandler(404)
 def not_found_error(error):
     return flask.render_template('errors/404.html'), 404
 
 
-@app.errors.bp.app_errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     app.db.session.rollback()
     return flask.render_template('errors/500.html'), 500
